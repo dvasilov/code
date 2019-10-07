0370644b41b810fb128cefb9c2928c210b88622d
Harry <hjwp2@cantab.net>
2019-10-07 14:44:25 +0100

    use bootstrap in redis [appendix_bootstrap_ends]

--- src/allocation/redis_pubsub.py
+++ src/allocation/redis_pubsub.py
@@ -2,31 +2,18 @@ import json
 import logging
 from dataclasses import asdict
 import redis
-
-from allocation import (
-    config, commands, events, messagebus, notifications, orm, unit_of_work,
-)
+from allocation import bootstrap, config, commands, events, messagebus
 
 logger = logging.getLogger(__name__)
 
 r = redis.Redis(**config.get_redis_host_and_port())
 
-def get_bus():
-    uow = unit_of_work.SqlAlchemyUnitOfWork()
-    bus = messagebus.MessageBus(
-        uow=uow,
-        notifications=notifications.EmailNotifications(),
-        publish=publish
-    )
-    uow.bus = bus
-    return bus
-
 
 def main():
     logger.info('Redis pubsub starting')
+    bus = bootstrap.bootstrap()
     pubsub = r.pubsub(ignore_subscribe_messages=True)
     pubsub.subscribe('change_batch_quantity')
-    bus = get_bus()
 
     for m in pubsub.listen():
         handle_change_batch_quantity(m, bus)
@@ -45,5 +32,4 @@ def publish(channel, event: events.Event):
 
 
 if __name__ == '__main__':
-    orm.start_mappers()
     main()
